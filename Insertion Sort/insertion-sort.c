8940d4e3d861cd57d93a8e1a5a3a3408f53e72bf
Abdullah Omar Nasif <naasif-nomaan@users.noreply.github.com>
2020-08-06 22:12:17 +0600

    Create insertion-sort.c

---
+++ Insertion Sort/insertion-sort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int step=0, item=0,n=0, temp=0, j=0;
+
+int arr[]={5,8,1,6};
+
+n=sizeof(arr)/sizeof(int);
+
+for(int i=0; i<n;i++){
+
+
+  step=i;
+   j=i;
+ while(step>-1){
+
+ if( arr[j]>arr[j+1]){
+
+   temp=arr[j];
+   arr[j]=arr[j+1];
+   arr[j+1]=temp;
+
+
+   }
+step--;
+j--;
+}
+
+
+
+
+
+
+
+
+}
+
+for(int i=0; i<sizeof(arr)/sizeof(int);i++){
+
+printf("arr[%d]=%d \n",i,arr[i]);
+}
+
+    return 0;
+}
